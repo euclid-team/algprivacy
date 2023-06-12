00335f6aa5245370ae24018cb5863845d64a8b88
Nikolaos Pavlidis <62307313+nikopavl4@users.noreply.github.com>
2023-06-13 02:57:13 +0300

    New Model 1

--- team1/model.py
+++ team1/model.py
@@ -1,4 +1,5 @@
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 def get_model(seed):
-    return SVC(probability=True)
+    return RandomForestClassifier(max_depth=2, random_state=seed)
