4b8cbd4e838ae16b25c078c2075b08211cf6a1b5
Nikolaos Pavlidis <62307313+nikopavl4@users.noreply.github.com>
2023-06-20 16:58:57 +0300

    New models, trainsets

--- team1/model.py
+++ team1/model.py
@@ -1,5 +1,12 @@
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 
-def get_model(seed):
-    return RandomForestClassifier(max_depth=2, random_state=seed)
+def get_model(seed, name='forest'):
+    if name == 'forest':
+        return RandomForestClassifier(max_depth=2, random_state=seed)
+    elif name == 'logreg':
+        return LogisticRegression(solver="lbfgs",  random_state=seed)
+    elif name == 'knn':
+        return KNeighborsClassifier(n_neighbors=3)
